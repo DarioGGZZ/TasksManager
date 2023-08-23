3b6cc2b2a635b9121c1f2255c2f0bfa747b3bfc5
DarioGGZZ <dario.94.gomez@gmail.com>
2023-08-22 22:33:54 -0300

    US1: Se agrega el nuevo componente al main

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TaskList from './components/TasksList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.mainText}>TASK MANAGER</Text>
+      <TaskList></TaskList>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    fontWeight: 'bold', 
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
   },
+  listContainer: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  mainText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
